2de4a828cbe6ea03136470af21f7d02a670ac1f3
DischargedRobot <kolyaportov@yandex.ru>
2026-05-11 16:59:23 +0300

    add: migrations rename DisciplineId to DisciplineRegisterId in DisciplinesRegisters

---
+++ backend/MainService/Migrations/20260511112940_RenameIdDisiplinesRegistr.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MainService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameIdDisiplinesRegistr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisciplineId",
+                table: "DisciplinesRegisters",
+                newName: "DisciplineRegisterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisciplineRegisterId",
+                table: "DisciplinesRegisters",
+                newName: "DisciplineId");
+        }
+    }
+}
