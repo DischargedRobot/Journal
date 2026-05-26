b7ba48acd0a87bed6d5279ab8564b80f215cb35d
DischargedRobot <kolyaportov@yandex.ru>
2026-05-26 19:33:59 +0300

    Front: add: ClientThemeProvider

---
+++ frontend/journal/shared/lib/client-theme-provider/ClientThemeProvider.tsx
@@ -0,0 +1,38 @@
+"use client"
+import React from "react"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+declare module "@mui/material/styles" {
+	interface PaletteColor {
+		black?: PaletteOptions["primary"]
+	}
+
+	interface PaletteOptions {
+		black?: PaletteOptions["primary"]
+	}
+}
+
+const base = createTheme()
+
+const theme = createTheme({
+	palette: {
+		black: base.palette.augmentColor({
+			color: { main: base.palette.grey[900] },
+			name: "black",
+		}),
+		primary: { main: "#5b69e3" },
+		secondary: {
+			main: "#F3F3F3",
+			light: "#FCFCFC",
+			dark: "#EFEFEF",
+		},
+		mode: "light",
+	},
+})
+
+export default function ClientThemeProvider(props: {
+	children: React.ReactNode
+}) {
+	const { children } = props
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
---
+++ frontend/journal/shared/lib/client-theme-provider/index.ts
@@ -0,0 +1,1 @@
+export { default as ClientThemeProvider } from "./ClientThemeProvider"
